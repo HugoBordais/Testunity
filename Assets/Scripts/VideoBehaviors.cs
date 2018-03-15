efe9e20541769a43d2ea697c1148af20eb702b3d
HugoBordais <hugo.bordais@ynov.com>
2018-03-15 14:30:09 +0100

    first commit, video completed

---
+++ Assets/Scripts/VideoBehaviors.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+using Vuforia;
+
+public class VideoBehaviors : MonoBehaviour, ITrackableEventHandler {
+
+    // Use this for initialization
+    public GameObject ImageTarget;
+    private VideoPlayer _videoPlayer;
+    private StateManager _sm;
+    private TrackableBehaviour mTrackableBehaviour;
+    private bool _find;
+
+    void Start()
+    {
+        _videoPlayer = GetComponent<VideoPlayer>();
+        _sm = TrackerManager.Instance.GetStateManager();
+        mTrackableBehaviour = ImageTarget.GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            if (!_videoPlayer.isPlaying)
+                    _videoPlayer.Play();
+            
+            Debug.Log("_find = true");
+        }
+        else
+        {
+            _videoPlayer.Pause();
+            _find = false;
+            Debug.Log("_find = false");
+        }
+    }
+}
